95bddcfce29ecffb49d2332db52637bdcba12db4
alqaisi <yazraf2@gmail.com>
2019-12-23 23:55:34 +0100

    adding axios and test api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  axios.post('https://api.yaz-fr.com/api/login', {
+    email:'yazraf4@gmail.com',
+    password:'lozyaser_1985'
+  })
   return (
     <div className="App">
           <p>TestOOOOOOOOOO</p>
