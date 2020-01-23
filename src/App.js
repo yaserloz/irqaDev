ded5a584f1bf2b4d263252e98eec3528f245fc50
yasir ALQAISI <yasir.alqaisi@outlook.com>
2020-01-23 13:54:03 +0100

    adding semantic ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Header, Segment, Button, Image, List } from 'semantic-ui-react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-          <p>TestOOOOOOOOOO</p>
-    </div>
-  );
+	return (
+		<div>
+			<Header as="h3" block>
+				IraqiDev
+			</Header>
+			<List divided verticalAlign="left">
+				<List.Item>
+					<List.Content floated="right">
+						<Button>Add</Button>
+					</List.Content>
+					<Image avatar src="/images/avatar/small/lena.png" />
+					<List.Content>Lena</List.Content>
+				</List.Item>
+				<List.Item>
+					<List.Content floated="right">
+						<Button>Add</Button>
+					</List.Content>
+					<Image avatar src="/images/avatar/small/lindsay.png" />
+					<List.Content>Lindsay</List.Content>
+				</List.Item>
+				<List.Item>
+					<List.Content floated="right">
+						<Button>Add</Button>
+					</List.Content>
+					<Image avatar src="/images/avatar/small/mark.png" />
+					<List.Content>Mark</List.Content>
+				</List.Item>
+				<List.Item>
+					<List.Content floated="right">
+						<Button>Add</Button>
+					</List.Content>
+					<Image avatar src="/images/avatar/small/molly.png" />
+					<List.Content>Molly</List.Content>
+				</List.Item>
+			</List>
+		</div>
+	);
 }
 
 export default App;
